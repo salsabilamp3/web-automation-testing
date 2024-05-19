e0e4dd93e81a04f44fe18675964fc193dbda5c98
salsabilamp3 <salsabilamp3@gmail.com>
2024-05-19 14:46:29 +0700

    fixing test runner

--- src/test/java/swag/tests/TestRunner.java
+++ src/test/java/swag/tests/TestRunner.java
@@ -5,8 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/swag", glue = "swag/steps", plugin = { "pretty",
-                "html: testreport/test-reports.html" } // Generates HTML report in the testreport/test-reports directory
+@CucumberOptions(
+    features = "src/test/resources/swag", 
+    glue = "swag/steps", 
+    plugin = { "pretty", "html: testreport/test-reports.html" } // Generates HTML report in the testreport/test-reports directory
 )
 public class TestRunner {
 }
